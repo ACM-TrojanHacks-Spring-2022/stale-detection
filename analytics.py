8931a3b8eff57f94a0b26aa411fb7ab5863ca671
shubhamhegde <45269633+shubhamhegde@users.noreply.github.com>
2022-04-24 00:47:15 -0700

    json read and write

---
+++ analytics.py
@@ -0,0 +1,16 @@
+import json
+
+f = open('analytics_data.json','r')
+data = json.load(f)
+f.close()
+
+# get the classification based on prediction
+classification = 'Spoiled'
+
+data['pie_counts'][classification]+=1
+
+with open('analytics_data.json', 'w') as outfile:
+    json.dump(data, outfile)
+
+f.close()
+
